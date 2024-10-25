51b073725fb1b250fa1663e6faba064632807747
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-10-25 16:13:54 +0900

    chore: gradle, gitignore, yml

---
+++ src/main/java/com/meet2me/meet2me/Meet2meApplication.java
@@ -0,0 +1,13 @@
+package com.meet2me.meet2me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meet2meApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Meet2meApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meet2me/meet2me/Meet2meApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meet2me.meet2me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Meet2meApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
